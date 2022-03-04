9baf187ec367e51e59046f3b84352b1cf7777226
Krutarth Patel <krutarthzero@hotmail.com>
2022-03-04 16:48:51 -0400

    gogogo

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+fs = require("fs");
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  fs.writeFile("helloworld.txt", "Hello World!", function (err) {
+    if (err) return console.log(err);
+    console.log("Hello World > helloworld.txt");
+  });
+
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
