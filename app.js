b352e1bd9c3559d517f1fe5c0d2b74e0b5f94dbd
Krutarth Patel <krutarthzero@hotmail.com>
2022-03-04 20:03:12 -0400

    df

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const aws = require("aws-sdk");
+const axios = require("axios");
 fs = require("fs");
 
 const app = express();
@@ -6,13 +8,41 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  fs.writeFile("helloworld.txt", "Hello World!", function (err) {
-    if (err) return console.log(err);
-    console.log("Hello World > helloworld.txt");
+app.get("/send", async (req, res) => {
+  var inception = await axios.post("3.88.132.229/begin", {
+    banner: "B00896235",
+    ip: "3.86.227.244",
   });
 
-  res.json("Hello World!!!");
+  console.log(inception);
+});
+
+app.post("/storedata", async (req, res) => {
+  // console.log(req.body);
+  const s3 = new aws.S3({
+    accessKeyId: "ASIAWOU5GAJSX7MTD66R",
+    secretAccessKey: "LQrz3UrH9VeQWRieY+BOcm9ZNjxhXxiGbN0GX4WR",
+    sessionToken:
+      "FwoGZXIvYXdzEBAaDCCG5dar3M7Xj+EYZyLAAbASBZT69ePt+xVHS4Eis327DQlQvNKXp29L2pGuWiazSoPH+HAL147TUpDHZpGTLf5rGoq56LTP+sSu0IN6MsqAh+5J0oY1AzXIg/vttOCIMDOlZStMlqsUWF3xwi8Eml5MpTvTU4MEk1o+jdqBIveuz6DEsd+PrE6salVeTGuDWoKwIFOagUsSaypJfZ95bO4qJPt+CUQvSYNlE6MAYjSWdzl5sSt/d0KMJmZzYb9mm/d3nT/aTsdJnI6SfUAg7CjwpYqRBjIt3ORnW84k/4mug4TSQR5Y6Ol1ViwnKSBzNS/eSynYNRGXFTjINpy4q5j8zgdv",
+  });
+
+  s3.upload(
+    {
+      Bucket: "a2-advanced-cloud",
+      Key: "a2_res.txt",
+      Body: req.body.data,
+    },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(200).json({ s3uri: data.Location });
+    }
+  );
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "suuupp dude, are you lost ?" });
 });
 
 app.listen(port, () => {
