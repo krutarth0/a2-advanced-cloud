df53a2bf0aafa7ff6d120f6387d7261024bccbb3
Krutarth Patel <krutarthzero@hotmail.com>
2022-03-04 20:23:48 -0400

    dfg

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ app.get("/send", async (req, res) => {
       ip: "3.86.227.244",
     })
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       res.json({ log: data });
     })
     .catch(function (error) {
-      console.log(error);
+      // console.log(error);
       res.json({ errmsg: error.message, detailedError: error });
     });
 });
